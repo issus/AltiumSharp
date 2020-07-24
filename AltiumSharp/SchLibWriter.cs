fff42a7babfbfb4794ca7d321f6a827d9a523e94
ttrinidad <52244777+ttrinidad@users.noreply.github.com>
2020-07-24 07:25:01 -0300

    Implemented writing of SchLib files

---
+++ AltiumSharp/SchLibWriter.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AltiumSharp.BasicTypes;
+using AltiumSharp.Records;
+using OpenMcdf;
+
+namespace AltiumSharp
+{
+    /// <summary>
+    /// Schematic library reader.
+    /// </summary>
+    public sealed class SchLibWriter : SchWriter
+    {
+        /// <summary>
+        /// Header information for the schematic library file.
+        /// </summary>
+        public SchLibHeader Header { get; set; }
+
+        /// <summary>
+        /// List of component to be serialized to the file.
+        /// </summary>
+        public List<SchComponent> Components { get; }
+
+        public SchLibWriter(string fileName) : base(fileName)
+        {
+            Components = new List<SchComponent>();
+        }
+
+        protected override void DoWriteSch()
+        {
+            WriteFileHeader();
+            WriteSectionKeys();
+
+            foreach (var component in Components)
+            {
+                WriteComponent(component);
+            }
+        }
+
+        private void WriteSectionKeys()
+        {
+            // only write section keys for components that need them
+            var components = Components.Where(c => GetSectionKeyFromRefName(c.Name) != c.Name).ToList();
+            if (components.Count == 0) return;
+
+            var parameters = new ParameterCollection
+            {
+                { "KEYCOUNT", components.Count }
+            };
+
+            for (int i = 0; i < components.Count; ++i)
+            {
+                var component = components[i];
+                var componentRefName = component.Name;
+                var sectionKey = GetSectionKeyFromRefName(componentRefName);
+
+                parameters.Add($"LIBREF{i}", componentRefName);
+                parameters.Add($"SECTIONKEY{i}", sectionKey);
+            }
+
+            Cf.RootStorage.GetOrAddStream("SectionKeys").Write(writer =>
+            {
+                WriteBlock(writer, w => WriteParameters(w, parameters));
+            });
+        }
+
+        /// <summary>
+        /// Writes the "FileHeader" section which contains the list of components that
+        /// exist in the current library file.
+        /// </summary>
+        /// <returns></returns>
+        private void WriteFileHeader()
+        {
+            Cf.RootStorage.GetOrAddStream("FileHeader").Write(writer =>
+            {
+                // add the components to the header
+                Header.Comp.Clear();
+                Header.Comp.AddRange(Components.Select(c => (c.Name, c.Description, c.PartCount + 1)));
+
+                // write header
+                var parameters = Header.ExportToParameters();
+                WriteBlock(writer, w => WriteParameters(w, parameters));
+
+                // write the binary list of component ref names
+                writer.Write(Components.Count);
+                foreach (var component in Components)
+                {
+                    WriteStringBlock(writer, component.Name);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Writes a component to the adequate section key in the current file.
+        /// </summary>
+        /// <param name="component">Component to be serialized.</param>
+        private void WriteComponent(SchComponent component)
+        {
+            var resourceName = GetSectionKeyFromRefName(component.Name);
+            var componentStorage = Cf.RootStorage.GetOrAddStorage(resourceName);
+
+            var pinsWideText = new Dictionary<int, ParameterCollection>();
+            var pinsTextData = new Dictionary<int, byte[]>();
+            var pinsSymbolLineWidth = new Dictionary<int, ParameterCollection>();
+
+            component.AllPinCount = component.GetPrimitivesOfType<SchPin>().Count();
+
+            componentStorage.GetOrAddStream("Data").Write(writer =>
+            {
+                WriteComponentPrimitives(writer, component, pinsWideText, pinsTextData, pinsSymbolLineWidth);
+            });
+
+            WritePinTextData(componentStorage, pinsTextData);
+            WriteComponentExtendedParameters(componentStorage, "PinWideText", pinsWideText);
+            WriteComponentExtendedParameters(componentStorage, "PinSymbolLineWidth", pinsSymbolLineWidth);
+        }
+
+        /// <summary>
+        /// Writes a pin text data for the component at <paramref name="componentStorage"/>.
+        /// </summary>
+        private void WritePinTextData(CFStorage componentStorage, Dictionary<int, byte[]> data)
+        {
+            if (data.Count == 0) return;
+
+            componentStorage.GetOrAddStream("PinTextData").Write(writer =>
+            {
+                var parameters = new ParameterCollection
+                {
+                    { "HEADER", "PinTextData" },
+                    { "WEIGHT", data.Count }
+                };
+                WriteBlock(writer, w => WriteParameters(w, parameters));
+
+                foreach (var kv in data)
+                {
+                    WriteCompressedStorage(writer, kv.Key.ToString(), kv.Value);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Writes a component extended list of parameter at <paramref name="componentStorage"/>.
+        /// </summary>
+        /// <param name="componentStorage">Component storage.</param>
+        /// <param name="streamName">Name of the stream inside the component storage.</param>
+        /// <param name="data">Key, value set of parameters to store.</param>
+        private void WriteComponentExtendedParameters(CFStorage componentStorage, string streamName, Dictionary<int, ParameterCollection> data)
+        {
+            if (data.Count == 0) return;
+
+            componentStorage.GetOrAddStream(streamName).Write(writer =>
+            {
+                var parameters = new ParameterCollection
+                {
+                    { "HEADER", streamName },
+                    { "WEIGHT", data.Count }
+                };
+                WriteBlock(writer, w => WriteParameters(w, parameters));
+
+                foreach (var kv in data)
+                {
+                    WriteCompressedStorage(writer, kv.Key.ToString(), ws =>
+                        WriteBlock(ws, wb => WriteParameters(wb, kv.Value, true, Encoding.Unicode, false)));
+                }
+            });
+        }
+    }
+}
---
+++ AltiumSharp/SchWriter.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AltiumSharp.BasicTypes;
+using AltiumSharp.Records;
+
+namespace AltiumSharp
+{
+    public abstract class SchWriter : CompoundFileWriter
+    {
+        protected SchWriter(string fileName) : base(fileName)
+        {
+            EmbeddedImages = new Dictionary<string, Image>();
+        }
+
+        /// <summary>
+        /// Mapping of image file names to the actual image data for
+        /// embedded images.
+        /// </summary>
+        public Dictionary<string, Image> EmbeddedImages { get; }
+
+        protected sealed override void DoWrite()
+        {
+            DoWriteSch();
+            WriteStorageEmbeddedImages();
+        }
+
+        protected abstract void DoWriteSch();
+
+        /// <summary>
+        /// Writes embedded images from the "Storage" section of the file.
+        /// </summary>
+        protected void WriteStorageEmbeddedImages()
+        {
+            Cf.RootStorage.GetOrAddStream("Storage").Write(writer =>
+            {
+                var parameters = new ParameterCollection
+                {
+                    { "HEADER", "Icon storage" },
+                    { "WEIGHT", EmbeddedImages.Count }
+                };
+                WriteBlock(writer, w => WriteParameters(w, parameters));
+
+                foreach (var ei in EmbeddedImages)
+                {
+                    using (var imageData = new MemoryStream())
+                    {
+                        ei.Value.Save(imageData, ei.Value.RawFormat);
+                        WriteCompressedStorage(writer, ei.Key, imageData.ToArray());
+                    }
+                }
+            });
+        }
+
+        private static void WritePrimitive(BinaryWriter writer, SchPrimitive primitive,
+            int ownerIndex, ref int index, ref int pinIndex,
+            Dictionary<int, ParameterCollection> pinsWideText, Dictionary<int, byte[]> pinsTextData,
+            Dictionary<int, ParameterCollection> pinsSymbolLineWidth)
+        {
+            primitive.OwnerIndex = ownerIndex;
+
+            if (primitive is SchPin pin)
+            {
+                WritePinRecord(writer, pin, out var pinWideText, out var pinTextData, out var pinSymbolLineWidth);
+
+                if (pinTextData?.Length > 0)
+                {
+                    pinsTextData?.Add(pinIndex, pinTextData);
+                }
+                if (pinWideText?.Count > 0)
+                {
+                    pinsWideText?.Add(pinIndex, pinWideText);
+                }
+                if (pinSymbolLineWidth?.Count > 0)
+                {
+                    pinsSymbolLineWidth.Add(pinIndex, pinSymbolLineWidth);
+                }
+
+                pinIndex++;
+            }
+            else
+            {
+                WriteAsciiRecord(writer, primitive);
+            }
+
+            var currentIndex = index++;
+
+            foreach (var child in primitive.Primitives)
+            {
+                WritePrimitive(writer, child, currentIndex, ref index, ref pinIndex,
+                    pinsWideText, pinsTextData, pinsSymbolLineWidth);
+            }
+        }
+
+        protected static void WriteComponentPrimitives(BinaryWriter writer, SchComponent component,
+            Dictionary<int, ParameterCollection> pinsWideText, Dictionary<int, byte[]> pinsTextData,
+            Dictionary<int, ParameterCollection> pinsSymbolLineWidth)
+        {
+            var index = 0;
+            var pinIndex = 0;
+            WritePrimitive(writer, component, 0, ref index, ref pinIndex, pinsWideText, pinsTextData, pinsSymbolLineWidth);
+        }
+
+        /// <summary>
+        /// Writes an ASCII parameter based Record entry. 
+        /// </summary>
+        /// <param name="writer">Binary writer.</param>
+        /// <param name="primitive">Primitive to be serialized as a record.</param>
+        protected static void WriteAsciiRecord(BinaryWriter writer, SchPrimitive primitive)
+        {
+            var parameters = primitive.ExportToParameters();
+            WriteBlock(writer, w => WriteParameters(w, parameters), 0);
+        }
+
+        /// <summary>Checks if the string can be represented as ASCII</summary>
+        private static bool IsAscii(string s) => s == null || Encoding.UTF8.GetByteCount(s) == s.Length;
+
+        /// <summary>
+        /// Writes a binary SchPin Record entry. 
+        /// </summary>
+        /// <param name="writer">Binary writer.</param>
+        /// <param name="pin">Pin primitive to be serialized as a record.</param>
+        protected static void WritePinRecord(BinaryWriter writer, SchPin pin, out ParameterCollection pinWideText, out byte[] pinTextData, out ParameterCollection pinSymbolLineWidth)
+        {
+            WriteBlock(writer, w =>
+            {
+                pin.SymbolLineWidth = LineWidth.Smallest;
+
+                w.Write(pin.Record);
+                w.Write((byte)0); // TODO: unknown
+                w.Write((short)pin.OwnerPartId);
+                w.Write((byte)0); // TODO: unknown
+                w.Write((byte)pin.SymbolInnerEdge);
+                w.Write((byte)pin.SymbolOuterEdge);
+                w.Write((byte)pin.SymbolInside);
+                w.Write((byte)pin.SymbolOutside);
+                WritePascalShortString(w, pin.Description);
+                w.Write((byte)0); // TODO: unknown
+                w.Write((byte)pin.Electrical);
+                w.Write((byte)pin.PinConglomerate);
+                w.Write((short)Utils.CoordToDxpFrac(pin.PinLength).num);
+                w.Write((short)Utils.CoordToDxpFrac(pin.Location.X).num);
+                w.Write((short)Utils.CoordToDxpFrac(pin.Location.Y).num);
+                w.Write(ColorTranslator.ToWin32(pin.Color));
+                WritePascalShortString(w, pin.Name);
+                WritePascalShortString(w, pin.Designator);
+                w.Write((byte)0); // TODO: unknown
+                w.Write((byte)0); // TODO: unknown
+                w.Write((byte)0); // TODO: unknown
+            }, (byte)0x01); // flag needs to be 1
+
+            pinWideText = new ParameterCollection();
+            if (!IsAscii(pin.Description)) pinWideText.Add("DESC", pin.Description);
+            if (!IsAscii(pin.Name)) pinWideText.Add("NAME", pin.Name);
+            if (!IsAscii(pin.Designator)) pinWideText.Add("DESIG", pin.Designator);
+            
+            pinTextData = new byte[0];
+
+            pinSymbolLineWidth = new ParameterCollection
+            {
+                { "SYMBOL_LINEWIDTH", pin.SymbolLineWidth }
+            };
+        }
+    }
+}
