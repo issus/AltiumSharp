88b3d4f61896373fdae1744088fc468bc3049a7a
ttrinidad <52244777+ttrinidad@users.noreply.github.com>
2020-07-25 10:36:29 -0300

    Added SchLib.AddComponent() method to aid in the programmatic creation of new components

--- AltiumSharp/SchLib.cs
+++ AltiumSharp/SchLib.cs
@@ -1,4 +1,6 @@
-﻿using AltiumSharp.Records;
+﻿using System;
+using System.Linq;
+using AltiumSharp.Records;
 
 namespace AltiumSharp
 {
@@ -9,6 +11,28 @@ namespace AltiumSharp
         public SchLib() : base()
         {
 
+        }
+
+        private void AddComponent(SchComponent component)
+        {
+            if (string.IsNullOrEmpty(component.LibReference))
+            {
+                component.LibReference = $"Component_{Items.Count+1}";
+            }
+
+            var designator = component.GetPrimitivesOfType<DesignatorLabelRecord>(false).Where(r => r.Name == "Designator").SingleOrDefault();
+            if (designator == null)
+            {
+                component.Add(new DesignatorLabelRecord { Name = "Designator", ReadOnlyState = 1 });
+            }
+
+            var comment = component.GetPrimitivesOfType<SchParameter>(false).Where(r => r.Name == "Comment").SingleOrDefault();
+            if (designator == null)
+            {
+                component.Add(new SchParameter { Name = "Comment" });
+            }
+
+            Items.Add(component);
         }
     }
 }
